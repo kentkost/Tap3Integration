e54db5a9edff1ae0ed41c38979c4030c18d3bef2
Kent Kostelac <14237820+kentkost@users.noreply.github.com>
2023-09-30 04:40:12 +0200

    finished tap3.11 wrapper

--- Tap3Integration/Program.cs
+++ Tap3Integration/Program.cs
@@ -2,41 +2,52 @@
 using Tap3Integration;
 using static Tap3Integration.Tap3MagicWrapper;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Tap3Integration;
 
 class Program
 {
     static string inPath = @"E:/repos/tap3magic/build/debug-readers/tap3-sample-DataInterChange-3_11.ber";
-    static string outPath = @"E:/repos/tap3magic/build/debug-readers/results.xer";
-    static int inEncoding = (int)ASN1Encoding.BER;
-    static int outEncoding = (int)ASN1Encoding.XER;
+    static string outPath = @"E:/repos/tap3magic/build/debug-readers/resultssss.xer";
+    static ASN1Encoding inEncoding = ASN1Encoding.BER;
+    static ASN1Encoding outEncoding = ASN1Encoding.XER;
 
     static void Main(string[] args)
     {
-        //TestingFile2File(); // ✅
+        TestingFile2File(); // ✅
         //TestingBuffer2File(); // ✅
-        TestingBuffer2Buffer();
+        //TestingBuffer2Buffer(); // ✅
+        //TestingFile2Buffer(); // ✅
+        Console.WriteLine("Ended Successfully");
     }
-
 
     static void TestingFile2File()
     {
-        Tap3MagicWrapper.decode_tap0311_datainterchange_file2file(inEncoding, outEncoding, inPath, outPath);
+        Tap3MagicWrapper.DecodeFileToFile(inEncoding, outEncoding, inPath, outPath);
     }
 
     static void TestingBuffer2File()
     {
         var inputBuffer = File.ReadAllBytes(inPath);
         var length = Convert.ToUInt64(inputBuffer.Length);
-        Tap3MagicWrapper.decode_tap0311_datainterchange_buffer2file(inEncoding, outEncoding, inputBuffer, length, outPath);
+        Tap3MagicWrapper.DecodeBufferToFile(inEncoding, outEncoding, inputBuffer, length, outPath);
     }
 
     static void TestingBuffer2Buffer()
     {
         var inputBuffer = File.ReadAllBytes(inPath);
         var length = Convert.ToUInt64(inputBuffer.Length);
-        var res = Tap3MagicWrapper.decode_tap0311_datainterchange_buffer2buffer(inEncoding, outEncoding, inputBuffer, length);
-        //string str = Encoding.Default.GetString(res);
+        var encodedBytes = Tap3MagicWrapper.DecodeBufferToBuffer(inEncoding, outEncoding, inputBuffer, length);
+        var str = System.Text.Encoding.UTF8.GetString(encodedBytes);
+    }
+
+    static void TestingFile2Buffer()
+    {
+
+        var encodedBytes = Tap3MagicWrapper.DecodeFileToBuffer(inEncoding, outEncoding, inPath);
+
+        var str = System.Text.Encoding.UTF8.GetString(encodedBytes);
+
     }
 }
--- Tap3Integration/Tap3MagicWrapper.cs
+++ Tap3Integration/Tap3MagicWrapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Tap3Integration;
 
@@ -16,37 +18,166 @@ internal class Tap3MagicWrapper
     }
 
     /// <summary>
-    /// Read a file of any encoding and output to a new file with a new encoding.
+    /// Reads a file of any encoding and outputs a new file with a new encoding.
     /// </summary>
-    /// <param name="inputEncoding"></param>
-    /// <param name="outputEncoding"></param>
-    /// <param name="inFilePath"></param>
-    /// <param name="outFilePath"></param>
-    /// <returns></returns>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output file</param>
+    /// <param name="inFilePath">The file path for the input file</param>
+    /// <param name="outFilePath">The file path for the output file</param>
+    /// <returns>0 on success</returns>
     [DllImport("TestDll_d.dll", EntryPoint = "decode_tap0311_datainterchange_file2file", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    public static extern int decode_tap0311_datainterchange_file2file(int inputEncoding, int outputEncoding, string inFilePath, string outFilePath);
+    private static extern int decode_tap0311_datainterchange_file2file(int inputEncoding, int outputEncoding, string inFilePath, string outFilePath);
 
     /// <summary>
-    /// Read a buffer of any encoding and output to a new file with a new encoding
+    /// Reads a buffer of any encoding and outputs a new file with a new encoding
     /// </summary>
-    /// <param name="inputEncoding"></param>
-    /// <param name="outputEncoding"></param>
-    /// <param name="inputBuffer"></param>
-    /// <param name="inBufferSize"></param>
-    /// <param name="outFilePath"></param>
-    /// <returns></returns>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output file</param>
+    /// <param name="inputBuffer">The input buffer</param>
+    /// <param name="inBufferSize">The size of the input buffer</param>
+    /// <param name="outFilePath">The file path for the output file</param>
+    /// <returns>0 on success</returns>
     [DllImport("TestDll_d.dll", EntryPoint = "decode_tap0311_datainterchange_buffer2file", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    public static extern int decode_tap0311_datainterchange_buffer2file(int inputEncoding, int outputEncoding, byte[] inputBuffer, ulong inBufferSize, string outFilePath);
+    private static extern int decode_tap0311_datainterchange_buffer2file(int inputEncoding, int outputEncoding, byte[] inputBuffer, ulong inBufferSize, string outFilePath);
 
 
-    // Read a file of any encoding and output to new encoding.
+    /// <summary>
+    /// Reads a buffer and writes a new output buffer to an address
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output file</param>
+    /// <param name="inputBuffer">The input buffer</param>
+    /// <param name="inBufferSize">The size of the input buffer</param>
+    /// <param name="byteArrayPtr">The address that the output buffer is written to</param>
+    /// <returns>Number of bytes encoded to output buffer</returns>
     [DllImport("TestDll_d.dll", EntryPoint = "decode_tap0311_datainterchange_buffer2buffer", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    public static extern int decode_tap0311_datainterchange_buffer2buffer(int inputEncoding, int outputEncoding, byte[] inputBuffer, ulong inBufferSize);
+    private static extern int decode_tap0311_datainterchange_buffer2buffer(int inputEncoding, int outputEncoding, byte[] inputBuffer, ulong inBufferSize, ref IntPtr byteArrayPtr);
 
-
-    // Read a file of any encoding and output to new encoding.
+    /// <summary>
+    /// Reads a file and writes a new output buffer to an address
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output file</param>
+    /// <param name="inFilePath">The file path to the input buffer</param>
+    /// <param name="byteArrayPtr">The address that the output buffer is written to</param>
+    /// <returns>Number of bytes encoded to output buffer</returns>
     [DllImport("TestDll_d.dll", EntryPoint = "decode_tap0311_datainterchange_file2buffer", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    public static extern int decode_tap0311_datainterchange_file2buffer(int inputEncoding, int outputEncoding, string inFilePath);
+    private static extern int decode_tap0311_datainterchange_file2buffer(int inputEncoding, int outputEncoding, string inFilePath, ref IntPtr byteArrayPtr);
 
 
+    /// <summary>
+    /// Reads a file of any encoding and outputs a new file with a new encoding.
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output file</param>
+    /// <param name="inFilePath">The file path for the input file</param>
+    /// <param name="outFilePath">The file path for the output file</param>
+    /// <returns>0 on success</returns>
+    public static int DecodeFileToFile(ASN1Encoding inputEncoding, ASN1Encoding outputEncoding, string inFilePath, string outFilePath)
+    {
+        int inEnc = (int)inputEncoding;
+        int outEnc = (int)outputEncoding;
+        return Tap3MagicWrapper.decode_tap0311_datainterchange_file2file(inEnc, outEnc, inFilePath, outFilePath);
+    }
+
+    /// <summary>
+    /// Reads a buffer of any encoding and outputs a new file with a new encoding
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output file</param>
+    /// <param name="inputBuffer">The input buffer</param>
+    /// <param name="inputBufferSize">The size of the input buffer</param>
+    /// <param name="outFilePath">The file path for the output file</param>
+    /// <returns>0 on success</returns>
+    public static int DecodeBufferToFile(ASN1Encoding inputEncoding, ASN1Encoding outputEncoding, byte[] inputBuffer, ulong inputBufferSize, string outFilePath)
+    {
+        int inEnc = (int)inputEncoding;
+        int outEnc = (int)outputEncoding;
+        return Tap3MagicWrapper.decode_tap0311_datainterchange_buffer2file(inEnc, outEnc, inputBuffer, inputBufferSize, outFilePath);
+    }
+
+    /// <summary>
+    /// Reads a buffer and writes a new byte array
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output buffer</param>
+    /// <param name="inputBuffer">The input buffer</param>
+    /// <param name="inputBufferSize">The size of the input buffer</param>
+    /// <returns>A byte array corresponding to the output buffer</returns>
+    public static byte[] DecodeBufferToBuffer(ASN1Encoding inputEncoding, ASN1Encoding outputEncoding, byte[] inputBuffer, ulong inputBufferSize)
+    {
+        int inEnc = (int)inputEncoding;
+        int outEnc = (int)outputEncoding;
+        IntPtr byteArrayPtr = IntPtr.Zero;
+
+        var encodedBytes = Tap3MagicWrapper.decode_tap0311_datainterchange_buffer2buffer(inEnc, outEnc, inputBuffer, inputBufferSize, ref byteArrayPtr);
+
+        byte[] outBuffer = new byte[encodedBytes];
+        Marshal.Copy(byteArrayPtr, outBuffer, 0, encodedBytes);
+        return outBuffer;
+    }
+
+    /// <summary>
+    /// Reads a buffer and writes a string
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output string</param>
+    /// <param name="inputBuffer">The input buffer</param>
+    /// <param name="inputBufferSize">The size of the input buffer</param>
+    /// <returns>A string representation of the outout buffer. Helpful when output is XER or JER</returns>
+    public static string DecodeBufferToString(ASN1Encoding inputEncoding, ASN1Encoding outputEncoding, byte[] inputBuffer, ulong inputBufferSize)
+    {
+        int inEnc = (int)inputEncoding;
+        int outEnc = (int)outputEncoding;
+        IntPtr byteArrayPtr = IntPtr.Zero;
+
+        var encodedBytes = Tap3MagicWrapper.decode_tap0311_datainterchange_buffer2buffer(inEnc, outEnc, inputBuffer, inputBufferSize, ref byteArrayPtr);
+
+        byte[] outBuffer = new byte[encodedBytes];
+        Marshal.Copy(byteArrayPtr, outBuffer, 0, encodedBytes);
+        var str = System.Text.Encoding.UTF8.GetString(outBuffer);
+
+        return str;
+    }
+
+    /// <summary>
+    /// Reads a file and writes a string
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output string</param>
+    /// <param name="inFilePath">The file path for the input file</param>
+    /// <returns>A string representation of the outout buffer. Helpful when output is XER or JER</returns>
+    public static string DecodeFileToString(ASN1Encoding inputEncoding, ASN1Encoding outputEncoding, string inFilePath)
+    {
+        int inEnc = (int)inputEncoding;
+        int outEnc = (int)outputEncoding;
+        IntPtr byteArrayPtr = IntPtr.Zero;
+
+        var encodedBytes = Tap3MagicWrapper.decode_tap0311_datainterchange_file2buffer(inEnc, outEnc, inFilePath, ref byteArrayPtr);
+        byte[] outBuffer = new byte[encodedBytes];
+        Marshal.Copy(byteArrayPtr, outBuffer, 0, encodedBytes);
+        var str = System.Text.Encoding.UTF8.GetString(outBuffer);
+
+        return str;
+    }
+
+    /// <summary>
+    /// Reads a file and writes a byte array
+    /// </summary>
+    /// <param name="inputEncoding">The encoding of the input file</param>
+    /// <param name="outputEncoding">The encoding of the output array</param>
+    /// <param name="inFilePath">The file path for the input file</param>
+    /// <returns>A byte array corresponding to the output buffer</returns>
+    public static byte[] DecodeFileToBuffer(ASN1Encoding inputEncoding, ASN1Encoding outputEncoding, string inFilePath)
+    {
+        int inEnc = (int)inputEncoding;
+        int outEnc = (int)outputEncoding;
+        IntPtr byteArrayPtr = IntPtr.Zero;
+
+        var encodedBytes = Tap3MagicWrapper.decode_tap0311_datainterchange_file2buffer(inEnc, outEnc, inFilePath, ref byteArrayPtr);
+        byte[] outBuffer = new byte[encodedBytes];
+        Marshal.Copy(byteArrayPtr, outBuffer, 0, encodedBytes);
+        
+        return outBuffer;
+    }
 }
